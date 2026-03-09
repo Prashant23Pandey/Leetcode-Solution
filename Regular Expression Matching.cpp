e117f6b18ff54bbc1a7b4da400b789962b89afd6
Prashant Pandey <23prashantpandey@gmail.com>
2026-03-09 08:28:49 +0530

    Add files via upload

---
+++ Regular Expression Matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int stringLength = s.size();
+        int patternLength = p.size();
+        vector<vector<int>> memo(stringLength + 1, vector<int>(patternLength + 1, -1));
+        function<bool(int, int)> matchHelper = [&](int stringIdx, int patternIdx) -> bool {
+            if (patternIdx >= patternLength) {
+                return stringIdx == stringLength;
+            }
+            if (memo[stringIdx][patternIdx] != -1) {
+                return memo[stringIdx][patternIdx] == 1;
+            }
+            bool isMatchFound = false;
+            if (patternIdx + 1 < patternLength && p[patternIdx + 1] == '*') {
+                isMatchFound = matchHelper(stringIdx, patternIdx + 2) || 
+                              (stringIdx < stringLength && 
+                               (s[stringIdx] == p[patternIdx] || p[patternIdx] == '.') && 
+                               matchHelper(stringIdx + 1, patternIdx));  
+            }
+            else {
+                isMatchFound = stringIdx < stringLength && 
+                              (s[stringIdx] == p[patternIdx] || p[patternIdx] == '.') && 
+                              matchHelper(stringIdx + 1, patternIdx + 1);
+            }
+            memo[stringIdx][patternIdx] = isMatchFound ? 1 : 0;
+            return isMatchFound;
+        };
+        return matchHelper(0, 0);
+    }
+};
