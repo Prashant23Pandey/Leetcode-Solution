cbc5783779a8fef344033d08cd6684ead6815700
Prashant Pandey <23prashantpandey@gmail.com>
2026-04-03 08:48:46 +0530

    Add files via upload

---
+++ Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int writeIndex = 0;
+        for (int currentNum : nums) {
+            if (writeIndex == 0 || currentNum != nums[writeIndex - 1]) {
+                nums[writeIndex++] = currentNum;
+            }
+        }
+        return writeIndex;
+    }
+};
