d19e1606bddd87cd2693cb18766c3c369db9f52f
Prashant Pandey <23prashantpandey@gmail.com>
2026-04-15 00:03:12 +0530

    Add files via upload

---
+++ 45. Jump Game II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumpCount = 0;
+        int maxReach = 0;
+        int currentJumpEnd = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            maxReach = max(maxReach, i + nums[i]);
+            if (i == currentJumpEnd) {
+                ++jumpCount;
+                currentJumpEnd = maxReach;
+            }
+        }  
+        return jumpCount;
+    }
+};
