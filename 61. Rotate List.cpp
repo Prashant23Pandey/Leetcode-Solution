2fd27b44f2a0dcf82c877f30b0ebb9309ef786d7
Prashant Pandey <23prashantpandey@gmail.com>
2026-05-06 23:54:11 +0530

    Add files via upload

---
+++ 61. Rotate List.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* current = head;
+        int length = 0;
+        while (current) {
+            ++length;
+            current = current->next;
+        }
+        k %= length;
+        if (k == 0) {
+            return head;
+        }
+        ListNode* fastPointer = head;
+        ListNode* slowPointer = head;
+        while (k--) {
+            fastPointer = fastPointer->next;
+        }
+        while (fastPointer->next) {
+            fastPointer = fastPointer->next;
+            slowPointer = slowPointer->next;
+        }
+        ListNode* newHead = slowPointer->next;
+        slowPointer->next = nullptr;
+        fastPointer->next = head;
+        return newHead;
+    }
+};
