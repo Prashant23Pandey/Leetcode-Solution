8bf5450594ec92e6b05ac12526155bd5eb648e55
Prashant Pandey <23prashantpandey@gmail.com>
2026-05-18 23:49:58 +0530

    Add files via upload

---
+++ 74. Search a 2D Matrix.cpp
@@ -0,0 +1,15 @@
+	#include <vector>
+	using namespace std;
+	class Solution {
+	public:
+	    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+	        for (const auto& row : matrix) {
+	            for (int num : row) {
+	                if (num == target) {
+	                    return true;
+	                }
+	            }
+	        }
+	        return false;
+	    }
+	};
